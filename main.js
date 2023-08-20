0fc7d2df7c1aa83be11024654bd315249b6f23ea
Enmanuelhdr <ehdelrosario49@gmail.com>
2023-08-20 13:24:16 -0400

    finalitoo

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.querySelector('#app').innerHTML = `
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      FinaLES
+      Finalito
     </p>
   </div>
 `
