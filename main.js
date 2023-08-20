56cfef61729e33879f17553b8d9721b5e8c8ee24
Enmanuelhdr <ehdelrosario49@gmail.com>
2023-08-20 13:14:23 -0400

    Finalito

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.querySelector('#app').innerHTML = `
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Otro cambio otro mas uwu
+      Final
     </p>
   </div>
 `
