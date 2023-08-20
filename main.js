eeeb6f234d6f9f58f000da918b794de543208532
Enmanuelhdr <ehdelrosario49@gmail.com>
2023-08-20 11:37:46 -0400

    otro mas

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.querySelector('#app').innerHTML = `
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Otro cambio
+      Otro cambio otro mas uwu
     </p>
   </div>
 `
