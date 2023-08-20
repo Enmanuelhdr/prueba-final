7508ca5133c4327d3a67f2a54062110a9e8bf525
Enmanuelhdr <ehdelrosario49@gmail.com>
2023-08-20 14:39:01 -0400

    listoforo

---
+++ sendTelegramMessage.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+const MESSAGE = 'Hola desde GitHub Actions!';
+
+async function sendTelegramMessage() {
+    try {
+        const response = await axios.post(
+            `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+            {
+                chat_id: CHAT_ID,
+                text: MESSAGE
+            }
+        );
+        console.log('Mensaje enviado:', response.data);
+    } catch (error) {
+        console.error('Error al enviar el mensaje:', error.response.data);
+    }
+}
+
+sendTelegramMessage();
